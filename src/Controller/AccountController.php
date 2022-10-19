6756ca65d36e3a47d758506db497b4b84c2b7d10
MaurineDuray <maurine.duray@proximus.be>
2022-10-19 12:05:35 +0200

    cours 19 oct

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,29 +2,143 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
+use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountController extends AbstractController
 {
+    /**
+     * Permet à l'utilisateur de se connecter
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
     #[Route('/login', name: 'account_login')]
     public function index(AuthenticationUtils $utils): Response
     {
-
         $error = $utils->getLastAuthenticationError();
         $username = $utils->getLastUsername();
 
         return $this->render('account/index.html.twig', [
-            'has_error' => $error!==null,
-            'username'=>$username
+            'hasError' => $error !== null,
+            'username' => $username
         ]);
     }
 
+    /**
+     * Permet à l'utilisateur de se déconnecter
+     *
+     * @return void
+     */
     #[Route("/logout", name:"account_logout")]
-    public function logout():void
+    public function logout(): void
     {
-        //..
+        // ..
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'inscription d'un utilisateur et de l'ajouter à la base de données
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
+    #[Route("/register", name:"account_register")]
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $hash = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé"
+            );
+
+            return $this->redirectToRoute('account_login');
+
+        }
+
+        return $this->render("account/registration.html.twig",[
+            'myform' => $form->createView()
+        ]);
+    }
+
+    /**Editer les info du user connecté */
+    #[Route('/account/profile', name:"account_profile")]
+    public function profile(Request $request, EntityManagerInterface $manager):Response
+    {
+        $user = $this->getUser(); //récup l'utilisateur connecté
+        $form = $this->createForm(AccountType::class, $user); // car je ne veux pas les mots de passe
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+               'success',
+               "Les données ont été enregistrées avec succès"
+            );
+        }
+
+        return $this->render("account/profile.html.twig",[
+            'myform'=>$form->createView()
+        ]);
+    }
+
+    #[Route("/account/password-update", name:"account_password")]
+    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher):Response
+    {
+        $passwordUpdate = new PasswordUpdate();
+        $user = $this->getUser();
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            if(!password_verify($passwordUpdate->getOldPassword(),$user->getPassword())){ 
+                //les erreurs viennent de visual studio code du fait que ce n'est relié à aucune entité 
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez entré n'est pas votre mot de passe actuel"));
+            }else{
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $hasher->hashPassword($user, $newPassword);
+
+                $user -> setPassword($hash);
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifié"
+                );
+
+                return $this->redirectToRoute('homepage');
+            }
+        }
+        return $this->render("account/password.html.twig", [
+            'myform'=>$form->createView()
+        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,11 +8,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(fields:['email'],message:"Un autre utilisateur possède déjà cette adresse mail, merci de la modifier ")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -21,6 +24,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\Email(message: "Veuillez renseigner une adresse e-mail valide")]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -30,21 +34,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\Length(min:8, max:255, minMessage:'Vtre mdp doit être de plus de 8 caractères')]
     private ?string $password = null;
 
+    #[Assert\EqualTo(propertyPath:"password", message: "Vous n'avez pas confirmé correctement votre mot de passe")]
+    public ?string $passwordConfirm = null;
+
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Renseigner votre prénom")]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Renseigner votre nom")]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Url(message:"Donnez une url valide pour votre avatar")]
     private ?string $picture = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(min:10, max:255, minMessage:"Votre intro doit faire plus de 10 caractères", maxMessage:"Votre intro ne doit pas faire pus de 255 caractères")]
     private ?string $introduction = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\Length(min:100,  minMessage:"Votre intro doit faire plus de 10 caractères",)]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
