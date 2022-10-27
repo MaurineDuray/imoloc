40900196a61ca3e1b96011b6b98a664ee764277f
MaurineDuray <maurine.duray@skynet.be>
2022-10-27 16:10:03 +0200

    mis à jour

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\AccountType;
+use App\Entity\UserImgModify;
 use App\Entity\PasswordUpdate;
+use App\Form\ImgModifyType;
 use App\Form\RegistrationType;
 use App\Form\PasswordUpdateType;
 use Symfony\Component\Form\FormError;
@@ -13,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -64,6 +67,25 @@ class AccountController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid())
         {
+            // gestion de mon image
+            $file = $form['picture']->getData();
+            if(!empty($file))
+            {
+                $originalFilename = pathinfo($file->getClientOriginalName(),PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin;Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename."-".uniqid().".".$file->guessExtension();
+                try{
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                }catch(FileException $e)
+                {
+                    return $e->getMessage();
+                }
+                $user->setPicture($newFilename);
+            }
+
             $hash = $hasher->hashPassword($user, $user->getPassword());
             $user->setPassword($hash);
 
@@ -82,14 +104,22 @@ class AccountController extends AbstractController
         return $this->render("account/registration.html.twig",[
             'myform' => $form->createView()
         ]);
+
+
     }
 
-    /**Editer les info du user connecté */
-    #[Route('/account/profile', name:"account_profile")]
-    public function profile(Request $request, EntityManagerInterface $manager):Response
+    /**
+     * Permet d'éditer les informations d'un utilisateur connecté
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/profile", name:"account_profile")]
+    public function profile(Request $request, EntityManagerInterface $manager): Response
     {
-        $user = $this->getUser(); //récup l'utilisateur connecté
-        $form = $this->createForm(AccountType::class, $user); // car je ne veux pas les mots de passe
+        $user = $this->getUser(); // récup l'utilisateur connecté
+        $form = $this->createForm(AccountType::class, $user);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid())
@@ -98,18 +128,18 @@ class AccountController extends AbstractController
             $manager->flush();
 
             $this->addFlash(
-               'success',
-               "Les données ont été enregistrées avec succès"
+                'success',
+                "Les données ont été enregistrées avec succès"
             );
         }
 
         return $this->render("account/profile.html.twig",[
-            'myform'=>$form->createView()
+            'myform' => $form->createView()
         ]);
     }
 
-    #[Route("/account/password-update", name:"account_password")]
-    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher):Response
+    #[Route("/account/password-update", name:'account_password')]
+    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
     {
         $passwordUpdate = new PasswordUpdate();
         $user = $this->getUser();
@@ -118,14 +148,16 @@ class AccountController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid())
         {
-            if(!password_verify($passwordUpdate->getOldPassword(),$user->getPassword())){ 
-                //les erreurs viennent de visual studio code du fait que ce n'est relié à aucune entité 
-                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez entré n'est pas votre mot de passe actuel"));
+            // vérifier que le mot de passe correspond à l'ancien
+            if(!password_verify($passwordUpdate->getOldPassword(),$user->getPassword()))
+            {
+                // gérer l'erreur
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez tapé n'est pas votre mot de passe actuel"));
             }else{
                 $newPassword = $passwordUpdate->getNewPassword();
                 $hash = $hasher->hashPassword($user, $newPassword);
 
-                $user -> setPassword($hash);
+                $user->setPassword($hash);
                 $manager->persist($user);
                 $manager->flush();
 
@@ -137,8 +169,88 @@ class AccountController extends AbstractController
                 return $this->redirectToRoute('homepage');
             }
         }
-        return $this->render("account/password.html.twig", [
-            'myform'=>$form->createView()
+
+
+        return $this->render("account/password.html.twig",[
+            'myform' => $form->createView()
         ]);
+    }
+
+    /**
+     * Permet de modifier l'image de l'utilisateur
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/imgmodify", name:"account_modifimg")]
+    public function imgModify(Request $request, EntityManagerInterface $manager): Response
+    {
+        $imgModify = new UserImgModify();
+        $user = $this->getUser(); 
+        $form = $this->createForm(ImgModifyType::class, $imgModify);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // supprimer l'image dans le dossier
+            if(!empty($user->getPicture()))
+            {
+                unlink($this->getParameter('uploads_directory').'/'.$user->getPicture());
+            }
+
+            $file = $form['newPicture']->getData();
+            if(!empty($file))
+            {
+                $originalFilename = pathinfo($file->getClientOriginalName(),PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin;Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename."-".uniqid().".".$file->guessExtension();
+                try{
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                }catch(FileException $e)
+                {
+                    return $e->getMessage();
+                }
+                $user->setPicture($newFilename);
+            }
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre avatar a bien été modifié'
+            );
+
+            return $this->redirectToRoute('homepage');
+
+        }
+
+        return $this->render("account/imgModify.html.twig",[
+            'myform' => $form->createView()
+        ]);
+    }
+
+    #[Route("/account/delimg", name:'account_delimg')]
+    public function removeImg(EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser();
+        if(!empty($user->getPicture()))
+        {
+            unlink($this->getParameter('uploads_directory').'/'.$user->getPicture());
+            $user->setPicture('');
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addflash(
+                'success',
+                'Votre avatar a bien été supprimé'
+            );
+        }
+
+        return $this->redirectToRoute('homepage');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,20 +2,20 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Cocur\Slugify\Slugify;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\HasLifecycleCallbacks]
-#[UniqueEntity(fields:['email'],message:"Un autre utilisateur possède déjà cette adresse mail, merci de la modifier ")]
+#[UniqueEntity(fields:['email'], message: "Un autre utilisateur possède déjà cette adresse e-mail, merci de la modifier")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -34,30 +34,31 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-    #[Assert\Length(min:8, max:255, minMessage:'Vtre mdp doit être de plus de 8 caractères')]
+    #[Assert\Length(min: 8, max: 255, minMessage: "Votre mot de passe doit faire plus de 8 caractères")]
     private ?string $password = null;
 
-    #[Assert\EqualTo(propertyPath:"password", message: "Vous n'avez pas confirmé correctement votre mot de passe")]
+    #[Assert\EqualTo(propertyPath:"password", message:"Vous n'avez pas correctement confirmé votre mot de passe")]
     public ?string $passwordConfirm = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(message:"Renseigner votre prénom")]
+    #[Assert\NotBlank(message: "Vous devez renseigner votre prénom")]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(message:"Renseigner votre nom")]
+    #[Assert\NotBlank(message: "Vous devez renseigner votre nom de famille")]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Assert\Url(message:"Donnez une url valide pour votre avatar")]
+    #[Assert\Image(mimeTypes:["image/png","image/jpeg","image/jpg","image/gif"], mimeTypesMessage:"Vous devez upload un fichier jpg, jpeg, png ou gif")]
+    #[Assert\File(maxSize:"1024k", maxSizeMessage:"La taille du fichier est trop grande")]
     private ?string $picture = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\Length(min:10, max:255, minMessage:"Votre intro doit faire plus de 10 caractères", maxMessage:"Votre intro ne doit pas faire pus de 255 caractères")]
+    #[Assert\Length(min: 10, max: 255, minMessage:"Votre introduction doit faire plus de 10 caractères", maxMessage:"Votre introduction ne doit pas faire plus de 255 caractères")]
     private ?string $introduction = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Assert\Length(min:100,  minMessage:"Votre intro doit faire plus de 10 caractères",)]
+    #[Assert\Length(min: 100, minMessage:"Votre description doit faire plus de 100 caractères")]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
@@ -71,16 +72,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->ads = new ArrayCollection();
     }
 
-    /**Initialiser le slug auto */
+    /**
+     * Permet d'initialiser le slug automatiquement
+     *
+     * @return void
+     */
     #[ORM\PrePersist]
     #[ORM\PreUpdate]
     public function initializeSlug():void
     {
-        if(empty($this->slug)){
-
+        if(empty($this->slug))
+        {
             $slugify = new Slugify();
             $this->slug = $slugify->slugify($this->firstName.' '.$this->lastName.' '.uniqid());
-
         }
     }
 
